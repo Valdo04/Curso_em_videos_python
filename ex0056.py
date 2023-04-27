f0bf31b327bd5442ca514017561b8770ec6c18cf
Valdo04 <glacivaldo0408@gmail.com>
2023-04-27 00:02:28 -0300

    Update ex0056.py

--- ex0056.py
+++ ex0056.py
@@ -4,21 +4,44 @@
 # Qual é o nome do homem mais velho
 # Quantas mulheres têm menos de 20 anos.
 #####################################################################################################
-lt_masc = {}
+nomes_masc = []
+nomes_femi = []
+idade_masc = []
+idade_femi = []
 soma = 0
-maior = 0
-for p in range(1, 4):
+cont_id = 0
+
+for p in range(1, 5):
     nome = str(input('Digite seu nome: '))
     idade = int(input('Digite sua idade:'))
     sexo = int(input('''Digite seu sexo:
     [ 1 ] Masculino:
     [ 2 ] Feminino:
     :'''))
+
     if sexo == 1:
-        lt_masc[nome] = idade
+        nomes_masc.append(nome)
+        idade_masc.append(idade)
+    else:
+        nomes_femi.append(nome)
+        idade_femi.append(idade)
+
+soma += idade / 4
+mais_velho =  nomes_masc [0]
+maior_idade = idade_masc [0]
+
+for i in range(len(idade_masc)):
+    if idade_masc[i] > maior_idade:
+        maior_idade = idade_masc[i]
+        mais_velho = nomes_masc[i]
+
+maior_idade_f = idade_femi [0]
+
+for y in range(len(idade_femi)):
+    if idade_femi[y] < 20:
+        cont_id += 1
         
-     #soma += idade / 4
-homem_mais_velho = max(lt_masc, key=lambda k: lt_masc[k])
-lt_masc = max(nome)
-#print(f'A média de idade do grupo é {soma}')
-print(lt_masc[homem_mais_velho])
+
+print(f'A média de idade do grupo é {soma}')
+print(f'O homem mais velho é o {mais_velho}')
+print(f'{cont_id}  tem menos de 20 anos')
