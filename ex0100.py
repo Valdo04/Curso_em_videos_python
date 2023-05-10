522260522873c47a33b8506a0739045289db2218
Valdo04 <glacivaldo0408@gmail.com>
2023-05-09 23:55:11 -0300

    mais ex

---
+++ ex0100.py
@@ -0,0 +1,5 @@
+# Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar()
+# A primeira função vai sortea 5 numeros e vai colocá-los dentro da lista e a segunda função vai
+# mostra a soma entre todos ps valores PARES sorteados pela função anterior.
+###########################################################################################################
+
